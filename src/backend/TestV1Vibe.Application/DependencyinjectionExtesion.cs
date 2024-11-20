693f15e57002257385f2c3ce887dfab7e5865e9d
luzalex23 <alexluzendreyravi@gmail.com>
2024-11-20 11:26:42 -0300

    feat: implementation of logic in the Application layer, and validations of business rules and automapper

---
+++ src/backend/TestV1Vibe.Application/DependencyinjectionExtesion.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TestV1Vibe.Application.Services.Mapper;
+
+namespace TestV1Vibe.Application;
+
+public static class DependencyinjectionExtesion
+{
+    public static void AddApplication(this IServiceCollection services, IConfiguration configuration)
+    {
+        AddAutoMapper(services);
+    }
+
+    private static void AddAutoMapper(IServiceCollection services)
+    {
+        services.AddScoped(options => new AutoMapper.MapperConfiguration(options =>
+        {
+            options.AddProfile(new ApplicationMappingProfile());
+        }).CreateMapper());
+    }
+}
